e531ce562b262785d56f8b0bf34dc524e4395c26
Tahir Dibirov II <133943955+tahirdibirovsoftware@users.noreply.github.com>
2024-04-06 17:42:48 +0400

    Delete dist directory

--- dist/TYPES.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    ICustomerService: Symbol.for('ICustomerService'),
-    IOrderService: Symbol.for('IOrderService'),
-    CustomerController: Symbol.for('CustomerController'),
-    OrderController: Symbol.for('OrderController')
-};
-exports.default = TYPES;
--- dist/order/dto/order.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var OrderDto = /** @class */ (function () {
-    function OrderDto() {
-    }
-    return OrderDto;
-}());
-exports.default = OrderDto;
